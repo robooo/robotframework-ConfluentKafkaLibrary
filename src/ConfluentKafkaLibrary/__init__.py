0f5455f16916b308fc309195a48e26e76726f782
robooo <robo.karasek@gmail.com>
2019-06-04 13:04:07 +0200

    Refactoring and documentation for release 0.1.0

--- src/ConfluentKafkaLibrary/__init__.py
+++ src/ConfluentKafkaLibrary/__init__.py
@@ -3,6 +3,54 @@ from .version import VERSION
 
 
 class ConfluentKafkaLibrary(KafkaConsumer):
+    """ConfluentKafkaLibrary is a Robot Framework library which wraps up
+    [https://github.com/confluentinc/confluent-kafka-python | confluent-kafka-python].
+    Keywords are influenced by [https://github.com/s4int/robotframework-KafkaLibrary | robotframework-KafkaLibrary]
+    which is based on [https://github.com/Parsely/pykafka | pykafka project]
+    and support more functionality like running more clients based on `group_id` or running them in threaded mode
+    during the tests, decoding of gathered data etc. (`See` `Examples`).
+
+    This document explains how to use keywords provided by ConfluentKafkaLibrary.
+    For information about installation, support, and more, please visit the
+    [https://github.com/robooo/robotframework-ConfluentKafkaLibrary | project github page].
+    For more information about Robot Framework, see http://robotframework.org.
+
+    == Examples ==
+    
+    *Basic Consumer with predefined group_id*
+
+    | ${group_id}= | `Create Consumer` | group_id=mygroup | #if group_id is not defined uuid4() is gemerated |
+    | `Subscribe Topic` | group_id=${group_id} | topics=test_topic |
+    | ${result}= | `Poll` | group_id=${group_id} | max_records=5 |
+    | `Log` | ${result} |
+    | `Unsubscribe` | ${group_id} |
+
+    *More Consumers*
+
+    | ${group_id_1}= | `Create Consumer` |
+    | `Subscribe Topic` | group_id=${group_id_1} | topics=topic1 |
+    | ${group_id_2}= | `Create Consumer` |
+    | `Subscribe Topic` | group_id=${group_id_2} | topics=topic2 |
+    | ${result_1}= | `Poll` | group_id=${group_id_1} | max_records=5 |
+    | ${result_2}= | `Poll` | group_id=${group_id_2} | max_records=2 |
+    | `Unsubscribe` | ${group_id_1} |
+    | `Unsubscribe` | ${group_id_2} |
+
+    *Run Avro Consumer over HTTPS and threaded:*
+
+    | ${thread}= | `Start Messages Threaded` |
+    | | ...  topics=test_topic |
+    | | ...  schema_registry_url=https://myschema.io |
+    | | ...  auto_offset_reset=earliest | # We want to get all messages |
+    | | ...  security.protocol=ssl |
+    | | ...  schema.registry.ssl.ca.location=/home/user/cert/caproxy.pem |
+    | | ...  ssl.ca.location=/home/user/cert/caproxy.pem |
+    | | ...  ssl.certificate.location=/home/user/cert/kafka-client-cert.pem |
+    | | ...  ssl.key.location=/home/user/cert/kafka-client-key.pem |
+    | `Log` | `Execute commands which should push some data to topic` |
+    | ${messages}= | `Get Messages From Thread` | ${thread} |
+
+    """
+
     ROBOT_LIBRARY_VERSION = VERSION
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-
--- src/ConfluentKafkaLibrary/consumer.py
+++ src/ConfluentKafkaLibrary/consumer.py
@@ -11,19 +11,19 @@ from confluent_kafka.avro import AvroConsumer
 
 class GetMessagesThread(Thread):
 
-    def __init__(self,
-                 server='127.0.0.1',
-                 port='9092',
-                 topics='',
-                 group_id='',
-                 func_to_run=None,
-                 **kwargs
-                 ):
+    def __init__(
+        self,
+        server='127.0.0.1',
+        port='9092',
+        topics='',
+        group_id='',
+        **kwargs
+    ):
 
         super(GetMessagesThread, self).__init__()
         self.daemon = True
         self.consumer = KafkaConsumer()
-        self.group_id = getattr(self.consumer, func_to_run)(server=server, port=port, group_id=group_id, **kwargs)
+        self.group_id = self.consumer.create_consumer(server=server, port=port, group_id=group_id, **kwargs)
         if not isinstance(topics, list):
             topics = [topics]
         self.consumer.subscribe_topic(self.group_id, topics=topics)
@@ -44,46 +44,65 @@ class KafkaConsumer(object):
     def __init__(self):
         self.__consumers = {}
 
-    def create_consumer(self,
-                        server="127.0.0.1",
-                        port="9092",
-                        group_id=str(uuid.uuid4()),
-                        enable_auto_commit=True,
-                        auto_offset_reset="latest",
-                        **kwargs
-                        ):
+    def create_consumer(
+        self,
+        server="127.0.0.1",
+        port="9092",
+        client_id='Robot',
+        group_id=str(uuid.uuid4()),
+        enable_auto_commit=True,
+        auto_offset_reset="latest",
+        schema_registry_url=None,
+        **kwargs
+    ):
+        """Create Kafka Consumer and returns its `group_id` as string.
 
-        consumer = Consumer({
-            'bootstrap.servers': '{}:{}'.format(server, port),
-            'group.id': group_id,
-            'enable.auto.commit': enable_auto_commit,
-            'default.topic.config': {
-                'auto.offset.reset': auto_offset_reset
-            },
-            **kwargs
-        })
+        Keyword Arguments:
+        - ``server``: (str): IP address / domain, that the consumer should
+            contact to bootstrap initial cluster metadata.
+            Default: `127.0.0.1`.
+        - ``port`` (int): Port number. Default: `9092`.
+        - ``client_id`` (str): a name for this client. This string is passed in
+            each request to servers and can be used to identify specific
+            server-side log entries that correspond to this client. Also
+            submitted to GroupCoordinator for logging with respect to
+            consumer group administration. Default: `Robot`.
+        - ``group_id`` (str or uuid.uuid4() if not set) : name of the consumer group to join for dynamic
+            partition assignment (if enabled), and to use for fetching and
+            committing offsets. If None, unique string is generated  (via uuid.uuid4())
+            and offset commits are disabled. Default: `None`.
+        - ``auto_offset_reset`` (str): A policy for resetting offsets on
+            OffsetOutOfRange errors: `earliest` will move to the oldest
+            available message, `latest` will move to the most recent. Any
+            other value will raise the exception. Default: `latest`.
+        - ``enable_auto_commit`` (bool): If true the consumer's offset will be
+            periodically committed in the background. Default: `True`.
+        - ``schema_registry_url`` (str): *required* for Avro Consumer. Full URL to avro schema endpoint.
 
-        self.__consumers[group_id] = consumer
-        return group_id
+        Note:
+        Configuration parameters are described in more detail at
+        https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md \n
+        """
 
-    def create_avro_consumer(self,
-                             server="127.0.0.1",
-                             port="9092",
-                             group_id=str(uuid.uuid4()),
-                             enable_auto_commit=True,
-                             auto_offset_reset='latest',
-                             **kwargs
-                             ):
-
-        consumer = AvroConsumer({
-            'bootstrap.servers': '{}:{}'.format(server, port),
-            'group.id': group_id,
-            'enable.auto.commit': enable_auto_commit,
-            'default.topic.config': {
-                'auto.offset.reset': auto_offset_reset
-            },
-            **kwargs
-        })
+        if schema_registry_url:
+            consumer = AvroConsumer({
+                'bootstrap.servers': '{}:{}'.format(server, port),
+                'group.id': group_id,
+                'enable.auto.commit': enable_auto_commit,
+                'default.topic.config': {
+                    'auto.offset.reset': auto_offset_reset
+                },
+                'schema.registry.url': schema_registry_url,
+                **kwargs})
+        else:
+            consumer = Consumer({
+                'bootstrap.servers': '{}:{}'.format(server, port),
+                'group.id': group_id,
+                'enable.auto.commit': enable_auto_commit,
+                'default.topic.config': {
+                    'auto.offset.reset': auto_offset_reset
+                },
+                **kwargs})
 
         self.__consumers[group_id] = consumer
         return group_id
@@ -94,7 +113,7 @@ class KafkaConsumer(object):
                 return True
         return False
 
-    def assign_to_topic_partition(self, group_id, topic_partition=None):
+    def assign_to_topic_partition(self, group_id, topic_partition):
         """Assign a list of TopicPartitions.
 
         - ``partitions`` (list of `TopicPartition`): Assignment for this instance.
@@ -105,7 +124,7 @@ class KafkaConsumer(object):
         if not self._is_assigned(group_id, topic_partition):
             self.__consumers[group_id].assign(topic_partition)
 
-    def subscribe_topic(self, group_id, topics=[]):
+    def subscribe_topic(self, group_id, topics):
         """Subscribe to a list of topics, or a topic regex pattern.
 
         - ``topics`` (list): List of topics for subscription.
@@ -121,21 +140,24 @@ class KafkaConsumer(object):
         self.__consumers[group_id].unsubscribe()
 
     def close_consumer(self, group_id):
+        """Close down and terminate the Kafka Consumer.
+        """
         self.__consumers[group_id].close()
 
     def poll(
         self,
-        group_id=None,
+        group_id,
         timeout=1,
         max_records=1,
         poll_attempts=10
     ):
-
-        """Fetch messages from assigned topics / partitions.
+        """Fetch and return messages from assigned topics / partitions as list.
         - ``max_records`` (int): maximum number of messages to get from poll. Default: 1.
         - ``timeout`` (int): Seconds spent waiting in poll if data is not available in the buffer.
         If 0, returns immediately with any records that are available currently in the buffer, else returns empty.
         Must not be negative. Default: `1`
+        -  ``poll_attempts`` (int): Attempts to consume messages and endless looping prevention.
+        Sometimes the first messages are None or the topic could be empty. Default: `10`.
         """
 
         messages = []
@@ -167,6 +189,8 @@ class KafkaConsumer(object):
 
     # Experimental keywords
     def decode_data(self, data, decode_format, remove_zero_bytes):
+        """ TODO
+        """
         if decode_format and remove_zero_bytes:
             return [record.decode(str(decode_format)).replace('\x00', '') for record in data]
         elif decode_format and not remove_zero_bytes:
@@ -177,26 +201,36 @@ class KafkaConsumer(object):
             return data
 
     # Experimental - getting messages from kafka topic every second
-    def start_messages_threaded(
+    def start_consumer_threaded(
         self,
+        group_id,
+        topics,
         server='127.0.0.1',
         port='9092',
-        group_id=None,
-        topics='',
-        func_to_run='create_consumer',
         **kwargs
     ):
 
         if group_id is None:
             group_id = str(uuid.uuid4())
 
-        t = GetMessagesThread(server, port, topics, group_id=group_id, func_to_run=func_to_run, **kwargs)
+        t = GetMessagesThread(server, port, topics, group_id=group_id, **kwargs)
         t.start()
         t.join()
         return t
 
     def get_messages_from_thread(self, running_thread, decode_format=None, remove_zero_bytes=False):
-        records = self.decode_data(data=running_thread.messages, decode_format=decode_format, remove_zero_bytes=remove_zero_bytes)
+        """ Returns all records gathered from specific thread
+            - ``running_thread`` (Thread object) - thread which was executed by `Start Consumer Threaded`
+            - ``decode_data`` (str) - If you need to decode data to specific format
+                (See https://docs.python.org/3/library/codecs.html#standard-encodings). Default: None.
+            - ``remove_zero_bytes`` (bool) - When you are working with byte streams
+                you can end up with a lot of '\\x00' bytes you want to remove. Default: False.
+        """
+        records = self.decode_data(
+            data=running_thread.messages,
+            decode_format=decode_format,
+            remove_zero_bytes=remove_zero_bytes
+        )
         return records
 
     def stop_thread(self, running_thread):
