5140bbf2c7a64e0d94dd54447c587a58f11bd033
Todor Minakov <todor.minakov@xogito.com>
2020-10-15 18:26:23 +0300

    remove the package as dependency, it's in the standard library

--- setup.py
+++ setup.py
@@ -28,8 +28,7 @@ setup(name         = 'robotframework-confluentkafkalibrary',
       ],
       install_requires = [
           'robotframework >= 3.2.1',
-          'confluent-kafka == 1.5.0',
-          'uuid==1.30',
+          'confluent-kafka == 1.5.0'
       ],
       package_dir  = {'' : 'src'},
       packages    = ['ConfluentKafkaLibrary'],
