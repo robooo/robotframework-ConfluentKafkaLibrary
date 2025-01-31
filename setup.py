67991ccce139de2df15cf694101066679cd72a38
Róbert Karásek <robo.karasek@gmail.com>
2025-01-31 23:40:16 +0100

    Add missing SCHEMA_REGISTRY requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ AVRO_REQUIRES = ['fastavro >= 1.3.2', 'avro >= 1.11.1']
 LEGACYAVRO_REQUIRES = ['fastavro >= 1.3.2', 'avro-python3 >= 1.10.1']
 JSON_REQUIRES = ['jsonschema >= 3.2.0']
 PROTO_REQUIRES = ['protobuf >= 4.22.0']
-SCHEMA_REGISTRY_REQUIRES = ['httpx>=0.26']
+SCHEMA_REGISTRY_REQUIRES = ['httpx>=0.26', 'cachetools >= 5.5.0', 'attr >= 0.3.2']
 ALL = AVRO_REQUIRES + LEGACYAVRO_REQUIRES + JSON_REQUIRES + PROTO_REQUIRES + SCHEMA_REGISTRY_REQUIRES
 setup(name         = 'robotframework-confluentkafkalibrary',
       version      = VERSION,
